b8a55d5d507e3f072bc9d1621ab17dce4d05c6ad
Olli Karppinen <olli.karppinen@relex.fi>
2019-04-07 01:24:05 -0700

    Updates cli

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -3,33 +3,33 @@ package main
 
 import (
 
-	// Import the generated protobuf code
-	"fmt"
+  // Import the generated protobuf code
+  "fmt"
 
-	pb "./proto/consignment"
-	micro "github.com/micro/go-micro"
-	"golang.org/x/net/context"
+  pb "github.com/ollikarppinen/micro-go/consignment-service/proto/consignment"
+  micro "github.com/micro/go-micro"
+  "golang.org/x/net/context"
 )
 
 type IRepository interface {
-	Create(*pb.Consignment) (*pb.Consignment, error)
-	GetAll() []*pb.Consignment
+  Create(*pb.Consignment) (*pb.Consignment, error)
+  GetAll() []*pb.Consignment
 }
 
 // Repository - Dummy repository, this simulates the use of a datastore
 // of some kind. We'll replace this with a real implementation later on.
 type Repository struct {
-	consignments []*pb.Consignment
+  consignments []*pb.Consignment
 }
 
 func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	updated := append(repo.consignments, consignment)
-	repo.consignments = updated
-	return consignment, nil
+  updated := append(repo.consignments, consignment)
+  repo.consignments = updated
+  return consignment, nil
 }
 
 func (repo *Repository) GetAll() []*pb.Consignment {
-	return repo.consignments
+  return repo.consignments
 }
 
 // Service should implement all of the methods to satisfy the service
@@ -37,7 +37,7 @@ func (repo *Repository) GetAll() []*pb.Consignment {
 // in the generated code itself for the exact method signatures etc
 // to give you a better idea.
 type service struct {
-	repo IRepository
+  repo IRepository
 }
 
 // CreateConsignment - we created just one method on our service,
@@ -45,46 +45,46 @@ type service struct {
 // argument, these are handled by the gRPC server.
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
 
-	// Save our consignment
-	consignment, err := s.repo.Create(req)
-	if err != nil {
-		return err
-	}
+  // Save our consignment
+  consignment, err := s.repo.Create(req)
+  if err != nil {
+    return err
+  }
 
-	// Return matching the `Response` message we created in our
-	// protobuf definition.
-	res.Created = true
-	res.Consignment = consignment
-	return nil
+  // Return matching the `Response` message we created in our
+  // protobuf definition.
+  res.Created = true
+  res.Consignment = consignment
+  return nil
 }
 
 func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
-	consignments := s.repo.GetAll()
-	res.Consignments = consignments
-	return nil
+  consignments := s.repo.GetAll()
+  res.Consignments = consignments
+  return nil
 }
 
 func main() {
 
-	repo := &Repository{}
+  repo := &Repository{}
 
-	// Create a new service. Optionally include some options here.
-	srv := micro.NewService(
+  // Create a new service. Optionally include some options here.
+  srv := micro.NewService(
 
-		// This name must match the package name given in your protobuf definition
-		micro.Name("go.micro.srv.consignment"),
-		micro.Version("latest"),
-	)
+    // This name must match the package name given in your protobuf definition
+    micro.Name("go.micro.srv.consignment"),
+    micro.Version("latest"),
+  )
 
-	// Init will parse the command line flags.
-	srv.Init()
+  // Init will parse the command line flags.
+  srv.Init()
 
-	// Register handler
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+  // Register handler
+  pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
 
-	// Run the server
-	if err := srv.Run(); err != nil {
-		fmt.Println(err)
-	}
+  // Run the server
+  if err := srv.Run(); err != nil {
+    fmt.Println(err)
+  }
 }
 
